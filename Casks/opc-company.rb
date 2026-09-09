5f1522f01baecf138dafc1e319739c75e27e87a0
B1ueMu3ic4m <134594486+B1ueMu3ic4m@users.noreply.github.com>
2026-09-10 00:08:54 +0800

    opc-company 0.2.0: headless CLI shipped in-bundle; new asset sha

--- Casks/opc-company.rb
+++ Casks/opc-company.rb
@@ -1,6 +1,6 @@
 cask "opc-company" do
-  version "0.1.0"
-  sha256 "d65cc223d8406b9fcc5d2361317d356a7c565462241c9a47c7d8dbee3db1e4dc"
+  version "0.2.0"
+  sha256 "4d91a70b84224ab130a97863e162d5f6dceeb7d72f2ceb1c41e3f349ddf2e8c2"
 
   url "https://github.com/B1ueMu3ic4m/OPCCompany/releases/download/v#{version}/OPCCompany-v#{version}.zip"
   name "OPC Company"
