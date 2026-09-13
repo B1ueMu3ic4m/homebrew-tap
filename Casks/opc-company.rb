5f54739e9d32082049487ee900830f6ebf4bbe24
B1ueMu3ic4m <134594486+B1ueMu3ic4m@users.noreply.github.com>
2026-09-13 13:18:53 +0800

    opc-company 0.2.1: Windows CI milestone release; asset sha sync

--- Casks/opc-company.rb
+++ Casks/opc-company.rb
@@ -1,6 +1,6 @@
 cask "opc-company" do
-  version "0.2.0"
-  sha256 "4d91a70b84224ab130a97863e162d5f6dceeb7d72f2ceb1c41e3f349ddf2e8c2"
+  version "0.2.1"
+  sha256 "3a7fee5e1b3a796f495e31d0c1b997927d5f05cda2095cf34624ba0e84f5f69c"
 
   url "https://github.com/B1ueMu3ic4m/OPCCompany/releases/download/v#{version}/OPCCompany-v#{version}.zip"
   name "OPC Company"
