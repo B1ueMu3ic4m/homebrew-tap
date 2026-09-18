03a5761850ab1dcd534541b61a3e15cd8f90dba8
B1ueMu3ic4m <134594486+B1ueMu3ic4m@users.noreply.github.com>
2026-09-18 08:39:46 +0800

    opc-company: tidy sha256 alignment

--- Casks/opc-company.rb
+++ Casks/opc-company.rb
@@ -1,6 +1,6 @@
 cask "opc-company" do
   version "0.3.3"
-  sha256  "8b0f0f3c6af8fc54add53d5e6e71e9a5cc22bca4cd6cca06b7058d6ba7aa7d35"
+  sha256 "8b0f0f3c6af8fc54add53d5e6e71e9a5cc22bca4cd6cca06b7058d6ba7aa7d35"
 
   url "https://github.com/B1ueMu3ic4m/OPCCompany/releases/download/v#{version}/OPCCompany-v#{version}.zip"
   name "OPC Company"
